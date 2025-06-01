c479b0248f98ad7326eb18632e61071c9b5eb25c
wakamonoo <joven.serdanbataller21@gmail.com>
2025-06-01 23:58:58 +0800

    pwa

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "todochan-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/script.js",
+  "/images/logo.png",
+  "/manifest.json",
+  "https://cdn.tailwindcss.com",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      )
+    )
+  );
+});
